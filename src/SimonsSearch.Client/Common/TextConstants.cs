7703b4a883471e50e100b14371db037c085e55fe
rcolladoapaleo <randy.collado@apaleo.com>
2020-12-24 02:39:54 +0100

    Add text constants

---
+++ src/SimonsSearch.Client/Common/TextConstants.cs
@@ -0,0 +1,9 @@
+namespace SimonsSearch.Client.Common
+{
+    public static class TextConstants
+    {
+        public const string NameCellText = "Name";
+        public const string TypeCellText = "Result Type";
+        public const string InformationCellText = "Information";
+    }
+}
